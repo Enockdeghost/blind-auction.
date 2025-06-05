e0d1604fb284ec868aba5f645d4f01a76ab7fb60
enock_stephano <enockdeghost@gmail.com>
2025-06-05 16:14:00 +0300

    input validator

--- blind_auction.py
+++ blind_auction.py
@@ -10,15 +10,30 @@ def clear_screen():
         subprocess.call("clear")
 bids = {}
 bidding_finished = False
+
 while not bidding_finished:
     name = input("What is Your Name?: ")
-    price =float(input("What is Ammount You want to Bid? Tsh"))
+    # hakuna kueka ujinga
+    while True:
+        try:
+            price = float(input("What is Amount You want to Bid? Tsh "))
+            if price <= 0:
+                print("Please enter a positive amount!")
+                continue
+            break
+        except ValueError:
+            print("Please enter a valid number!")
+    
     bids[name] = price
-    finish_status = input("Are there other bidders? 'yes' or 'no': ")
+    finish_status = input("Are there other bidders? 'yes' or 'no': ").lower().strip()
+    
     if finish_status == "no":
         bidding_finished = True
     elif finish_status == 'yes':
-        subprocess.call("clear")
+        clear_screen()
+    else:
+        print('please answer yes/ no')
+        
 def find_highest_bidder(bidding_record):
     highest_ammount = 0
     winner = ""
